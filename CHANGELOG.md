f97b45d936e22f6294738d7e11f7580cab2d7267
llarreta <leonel@larreta.com.ar>
2021-11-17 09:22:25 -0300

    Commit inicial

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+
+* Version inicial del proyecto que permite integrar una aplicacion desarrollada en Flutter con la pasarela de pago de Mercado Pago
---
+++ example/android/app/src/main/java/ar/com/intrale/mobile/mercadopago_example/MainActivity.java
@@ -0,0 +1,6 @@
+package ar.com.intrale.mobile.mercadopago_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
